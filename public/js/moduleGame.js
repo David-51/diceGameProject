f2047aba6966e3cb235cd7aa8417ac6cf6cf64e8
David GRIGNON <david@davidgrignon.com>
2022-02-02 23:44:44 +0100

    FIX BUG and Factorise

---
+++ public/js/moduleGame.js
@@ -0,0 +1,85 @@
+import { Dice } from './moduleDice.js';
+import * as PLAYER from './modulePlayer.js';
+
+export class Game{
+    constructor(){        
+        this.status = 'outOfGame';
+        this.winnerAlert = new CustomEvent('winnerEvent');
+        this.startGameEvent = new CustomEvent('gameAlert', {
+            detail:{
+                name: 'start'
+            }
+        });      
+        this.dice = new Dice('dice');
+        this.dice.createDice();
+        this.dice.display();
+    }
+    setPlayerOne(name){
+        this.playerOne = new PLAYER.Player('playerOne', name);        
+    }
+    setPlayerTwo(name){
+        this.playerTwo = new PLAYER.Player('playerTwo', name);        
+    }
+
+    // Start new Game
+    newGame(){
+      this.playerOne.initPlayer();
+      this.playerTwo.initPlayer();
+      document.dispatchEvent(this.startGameEvent);   
+    }
+    rollDice(bool = false){     // cheatMode = true
+      if(bool === true){
+        return this.dice = 6;
+      }
+      this.dice = Math.floor(Math.random()*6)+1;    
+      return this.dice;
+    }
+  
+    play(diceRoll){        
+      if(diceRoll === 1){      
+        this.switchPlayer();
+      }
+      else{
+        console.log(diceRoll);
+        this.player().addToRound(diceRoll);
+      }
+    }
+    // Display Winner
+    winner(){
+      if(this.playerOne.global >= 100){
+        return this.playerOne.winner = true;
+      }
+      if(this.playerTwo.global >= 100){
+        return this.playerTwo.winner = true;
+      }
+    }  
+    player(){    
+      if(this.playerOne.turn === true){
+        return this.playerOne;
+      }
+      else{
+        return this.playerTwo;
+      }
+    }
+    switchPlayer(){
+      if(this.playerOne.turn === true){
+        console.log('this.playerOne.turn = ', this.playerOne.turn);
+        this.playerOne.setIsNotTurn();
+        this.playerTwo.initRound();
+        this.playerTwo.setIsTurn();
+      }
+      else{
+        this.playerOne.initRound();
+        this.playerOne.setIsTurn();
+        this.playerTwo.setIsNotTurn();
+      }
+    }
+    hold(){
+      this.player().addRoundToGlobal();
+      if(this.player().global >= 15){
+        document.dispatchEvent(this.winnerAlert);
+        return 
+      }
+      this.switchPlayer();
+    }
+  }
--- public/js/script.js
+++ public/js/script.js
@@ -1,178 +1,21 @@
 // jeu de dé
-import * as DICE from './diceAnim.js';
-
-class Player{
-  constructor(alias, name){
-    this.name = name;     
-    this.round = 0; // current round score
-    this.global = 0; // global score        
-    this.turn = false; // target the player who does play 
-    this.alias = alias;
-        
-    this.roundScoreEvent = new CustomEvent('scoreEvent', {
-      detail: {
-        player: this.name,
-        alias: this.alias,
-        name: 'Round'
-      }
-    })
-    this.globalScoreEvent = new CustomEvent('scoreEvent', {
-      detail: {
-        player: this.name,
-        alias: this.alias,
-        name: 'Global'
-      }
-    })
-    this.isTurnEvent = new CustomEvent('turnEvent',{
-      detail: {
-        name: 'isTurn',
-        player: this.name,
-        alias: this.alias
-      }
-    });    
-    this.isNotTurnEvent = new CustomEvent('turnEvent',{
-      detail: {
-        name: 'isNotTurn',
-        player: this.name,
-        alias: this.alias
-      }
-    });
-  }
-  setName(name){
-    this.name = name;
-  }
-
-  getName(){
-    return this.name;
-  }
-  Start(){
-    this.setIsTurn();
-    this.initRound();
-  }
-  setIsTurn(){
-    this.turn = true;
-    document.dispatchEvent(this.isTurnEvent);
-  }
-  setIsNotTurn(){
-    this.turn = false;
-    document.dispatchEvent(this.isNotTurnEvent);
-  }
-
-  initPlayer(){    
-    this.global = 0;    
-    this.setIsNotTurn();
-    this.initRound();    
-    document.dispatchEvent(this.globalScoreEvent);
-  }
-  
-  initRound(){    
-    this.round = 0;    
-    document.dispatchEvent(this.roundScoreEvent);
-  }
-
-  // Add to the score 
-  addToRound(dice){
-    this.round += dice;
-    document.dispatchEvent(this.roundScoreEvent);
-    return this.round;
-  }
-  
-  // "Hold" send ROUND to GLOBAL
-  addRoundToGlobal(){
-    this.global += this.round;        
-    document.dispatchEvent(this.globalScoreEvent);
-    return this.global;
-  }
-}
-
-class Game{
-  constructor(playerOne, playerTwo){
-    this.playerOne = new Player('playerOne', playerOne);
-    this.playerTwo = new Player('playerTwo', playerTwo);
-    this.winnerAlert = new CustomEvent('winnerEvent');
-    this.startGameEvent = new CustomEvent('gameAlert', {
-      detail:{
-        name: 'start'
-      }
-    });      
-  }
-  
-  // Start new Game
-  newGame(){
-    this.playerOne.initPlayer();
-    this.playerTwo.initPlayer();
-    document.dispatchEvent(this.startGameEvent);   
-  }
-  rollDice(bool = false){     // cheatMode = true
-    if(bool === true){
-      return this.dice = 6;
-    }
-    this.dice = Math.floor(Math.random()*6)+1;    
-    return this.dice;
-  }
-
-  play(diceRoll){        
-    if(diceRoll === 1){      
-      this.switchPlayer();
-    }
-    else{
-      console.log(diceRoll);
-      this.player().addToRound(diceRoll);
-    }
-  }
-  // Display Winner
-  winner(){
-    if(this.playerOne.global >= 100){
-      return this.playerOne.winner = true;
-    }
-    if(this.playerTwo.global >= 100){
-      return this.playerTwo.winner = true;
-    }
-  }  
-  player(){    
-    if(this.playerOne.turn === true){
-      return this.playerOne;
-    }
-    else{
-      return this.playerTwo;
-    }
-  }
-  switchPlayer(){
-    if(this.playerOne.turn === true){
-      console.log('this.playerOne.turn = ', this.playerOne.turn);
-      this.playerOne.setIsNotTurn();
-      this.playerTwo.initRound();
-      this.playerTwo.setIsTurn();
-    }
-    else{
-      this.playerOne.initRound();
-      this.playerOne.setIsTurn();
-      this.playerTwo.setIsNotTurn();
-    }
-  }
-  hold(){
-    this.player().addRoundToGlobal();
-    if(this.player().global >= 15){
-      document.dispatchEvent(this.winnerAlert);
-      return 
-    }
-    this.switchPlayer();
-  }
-}
+import * as GAME from './moduleGame.js';
 
 const formModal = new bootstrap.Modal(document.getElementById('formModal'));
 const winnerModal = new bootstrap.Modal(document.getElementById('winnerModal'));
 const form = document.getElementById('newGameForm');  
+const rollDiceButton = document.getElementById('rollDice');
+const holdbutton = document.getElementById('hold');
 
-const dice = new DICE.Dice('dice');
-dice.createDice();
-dice.display();
-let newGame;
+let newGame = new GAME.Game();
 
 form.addEventListener('submit', (event)=>{
-  event.preventDefault();            
-  newGame = new Game(form.playerOneInput.value, form.playerTwoInput.value);    
+  event.preventDefault();  
+  newGame.setPlayerOne(form.playerOneInput.value);
+  newGame.setPlayerTwo(form.playerTwoInput.value);  
+  newGame.status = 'inGame';
   newGame.newGame();
+
   formModal.toggle();
   document.getElementById('playerOneName').innerText = newGame.playerOne.name;
   document.getElementById('playerTwoName').innerText = newGame.playerTwo.name;
@@ -198,10 +41,7 @@ document.addEventListener('winnerEvent', () => {
   
   document.getElementById('congratulation').innerText = `Félicitations ${newGame.player().name}, vous avez gagné !`;
   winnerModal.show();
-  if(typeof newGame !== 'undefined' || newGame === null){
-    newGame = null; // à surveiller
-  }
-  console.log('deleted ?' + typeof newGame)
+  newGame.status = 'outOfGame';
 })
 
 document.addEventListener('turnEvent', (event) => {  
@@ -227,30 +67,31 @@ function displayActivePlayer(event){
   }
 }
 
-function randomFirstPlayer(Game){
+function randomFirstPlayer(newGame){
   let random = Math.floor(Math.random()*2);
   if(random == 0){
-    Game.playerOne.Start();
+    newGame.playerOne.Start();
   }
   else{
-    Game.playerTwo.Start();
+    newGame.playerTwo.Start();
   }
 }
 
-const rollDiceButton = document.getElementById('rollDice');
+
+
 rollDiceButton.addEventListener('click', () => {
-  if(typeof newGame === 'undefined'){
+  if(newGame.status !== 'inGame'){
     console.log('pas de partie en cours...')
   }
   else{
-    dice.play();    
+    newGame.dice.play();
   }
 })
 
-const holdbutton = document.getElementById('hold');
+
 holdbutton.addEventListener('click', () => {  
   console.log(typeof newGame);
-  if(typeof newGame ==='undefined' || newGame.player().round <= 0){
+  if(newGame.status !== 'inGame' || newGame.player().round <= 0){
     console.log('rien a hold')
   }
   else{
